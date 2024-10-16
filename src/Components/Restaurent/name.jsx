e255ab4c917f168de674df4d785fa69d4e3ea6ce
Sunilchow19 <you@example.com>
2024-10-16 22:14:29 +0530

    Second Commit

---
+++ src/Components/Restaurent/name.jsx
@@ -0,0 +1,15 @@
+
+import Desc from "./description";
+
+function Name(){
+    return (
+        <>
+        <h1><center><u>BS Restaurant</u></center></h1>
+        <Desc/>
+        </>
+        
+    )
+}
+
+export default Name;
+
